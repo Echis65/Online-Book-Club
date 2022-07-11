d4d3e93cac005c56bfdd35259291208209cbf6f9
Echis65 <evansatompoya@gmail.com>
2022-07-11 22:35:26 +0000

    Secured MongoDb credentials

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors"
+import dotenv from "dotenv"
 import userRoutes from "./Routes/userRoutes.js"
 
+dotenv.config()
 const app = express();
-const PORT = 5000;
-const CONNECTION_URL = 'mongodb+srv://Echis65:Echisviper65@cluster0.v9sjf1w.mongodb.net/onlineBookClub?retryWrites=true&w=majority'
+const PORT = process.env.PORT;
+
 
 app.use(bodyParser.json({limit : "30mb", extended:"true"}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended : true}))
@@ -14,9 +16,9 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended : true}))
 app.use(cors())
 app.get("/users", userRoutes)
 
-mongoose.connect(CONNECTION_URL)
-.then(() => {
-    console.log(`Server listening on port ${PORT}`)
+mongoose.connect(process.env.CONNECTION_URL)
+.then(() => { app.listen(PORT, () =>  console.log(`Server listening on port ${PORT}`))
+   
 }).catch(error => {
     console.log(error.message)
 })
